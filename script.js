c88cae09d073df4b4e6ced491895f2418bb78e7b
Lucas Pereira Cabral <234062189+lucascabral-dev@users.noreply.github.com>
2026-05-02 18:45:24 -0300

    Update

---
+++ script.js
@@ -0,0 +1,31 @@
+function digitando_auto(element){
+    const text = ">_ Python developer and Web developer."; 
+    // repare nos dois espaços iniciais
+    let i = 2; // começa do índice 2, assim os dois primeiros caracteres nunca são apagados
+    let apagando = false;
+
+    function escrever(){
+        if(!apagando && i < text.length){
+            element.textContent = text.substring(0, i+1);
+            i++;
+            setTimeout(escrever, 75);
+        } else if(!apagando && i === text.length){
+            setTimeout(() => {
+                apagando = true;
+                escrever();
+            }, 1500); // pausa no final
+        } else if(apagando && i > 2){ // não deixa apagar os dois primeiros caracteres
+            element.textContent = text.substring(0, i-1);
+            i--;
+            setTimeout(escrever, 50);
+        } else if(apagando && i === 2){
+            apagando = false;
+            setTimeout(escrever, 500);
+        }
+    }
+
+    escrever();
+}
+
+const titulo = document.querySelector('.digitando');
+digitando_auto(titulo);
